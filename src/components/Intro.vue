0449423c58c2107d571a15ec232268dc67709915
singuerinc <nahuel.scotti@gmail.com>
2017-05-08 22:09:38 +0200

    intro anim

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>TOMEITO</div>
+  <div><h1>{{appName}}</h1></div>
 </template>
 
 <script>
@@ -10,28 +10,63 @@
     router,
     data () {
       return {
+        appName: 'TOMEITO',
         isFade: false
       }
     },
     created () {
-      setTimeout(() => {
-        router.push('/tomatoes')
-      }, 2000)
+      let iterations = 0
+      const interval = setInterval(() => {
+        const alpha = '23456789'
+        const arr = []
+
+        let idx = 0
+        const r = Math.floor(iterations * 0.1)
+
+        do {
+          if (idx < r) {
+            arr.push('TOMEITO'[idx])
+          } else {
+            arr.push(alpha.charAt(Math.floor(Math.random() * alpha.length)))
+          }
+          idx++
+        }
+        while (idx < 7)
+        this.appName = arr.join('')
+        iterations++
+
+        if (iterations === 80) {
+          clearInterval(interval)
+          setTimeout(() => {
+            router.push('/tomatoes')
+          }, 1000)
+        }
+      }, 25)
     }
   }
 </script>
 <style scoped>
   div {
-    margin: 0;
-    padding-left: 8px;
-    line-height: 30px;
-    color: #999;
-    background: black;
-    height: 100%;
-    font-size: 16px;
+    position: absolute;
+    top: 0;
+    display: block;
+    width: 100%;
+    height: 30px;
+    background: #242424;
     opacity: 1;
     transition-property: opacity;
     transition-duration: 1000ms;
+  }
+
+  div h1 {
+    position: absolute;
+    font-size: 16px;
+    top: 5px;
+    left: 8px;
+    color: #999;
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
   }
 
   div small {
