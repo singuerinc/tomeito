011669e4d4248927582116fba8ea5145cf31759c
singuerinc <nahuel.scotti@gmail.com>
2020-04-19 15:34:39 +0200

    feat(states): init

--- packages/tomeito/src/app/Main.tsx
+++ packages/tomeito/src/app/Main.tsx
@@ -1,5 +1,55 @@
 import React from "react";
+import { useMachine } from "@xstate/react";
+import { Machine, interpret } from "xstate";
+import styled, { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const View = styled.div`
+  background-color: red;
+  width: 23em;
+  height: 3em;
+`;
+
+const stateMachine = Machine({
+  id: "state-machine",
+  initial: "timer",
+  context: {},
+  states: {
+    timer: {
+      initial: "long",
+      states: {
+        long: {
+          initial: "idle",
+          states: {
+            idle: {},
+            running: {}
+          }
+        },
+        break: {
+          initial: "idle",
+          states: {
+            idle: {},
+            running: {}
+          }
+        }
+      }
+    },
+    settings: {}
+  }
+});
 
 export function Main() {
-  return <div>main</div>;
+  const [state, send] = useMachine(stateMachine);
+  return (
+    <>
+      <Global />
+      <View>{JSON.stringify(state.value)}</View>
+    </>
+  );
 }
