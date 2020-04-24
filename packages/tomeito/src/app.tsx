3a90c3336994fa6b3949bdd7081b872f41939796
singuerinc <nahuel.scotti@gmail.com>
2020-04-24 22:40:45 +0200

    feat: tomato & break

--- packages/tomeito/src/app.tsx
+++ packages/tomeito/src/app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { Main } from "./app/Main";
+
 window.addEventListener("DOMContentLoaded", () => {
   render(<Main />, document.getElementById("app"));
 });
--- packages/tomeito/src/app/Timer.tsx
+++ packages/tomeito/src/app/Timer.tsx
@@ -1,11 +1,11 @@
 import { useMachine } from "@xstate/react";
+import format from "date-fns/format";
 import { icons } from "feather-icons";
 import React from "react";
-import { interval, Subscription } from "rxjs";
+import { interval } from "rxjs";
 import { map, take } from "rxjs/operators";
 import styled from "styled-components";
-import { assign, Machine, EventObject } from "xstate";
-import format from "date-fns/format";
+import { assign, Machine } from "xstate";
 
 type Tomato = {
   dateCreated: Date;
@@ -30,27 +30,58 @@ const startTimer = () => {
 };
 
 const timerMachine = Machine({
-  initial: "idle",
+  id: "timer",
+  initial: "tomato",
   context,
   states: {
-    idle: {
-      on: {
-        PLAY: "running"
+    tomato: {
+      initial: "idle",
+      states: {
+        idle: {
+          on: {
+            PLAY: "running"
+          }
+        },
+        running: {
+          invoke: {
+            src: () =>
+              interval(1000).pipe(
+                map(value => ({ type: "COUNT", value })),
+                take(60 * 25)
+              ),
+            onDone: "#timer.break"
+          },
+          on: {
+            COUNT: { actions: [assign((_, event) => ({ current: event.value }))] },
+            CANCEL: "#timer.break",
+            STOP: "#timer.break"
+          }
+        }
       }
     },
-    running: {
-      invoke: {
-        src: () =>
-          interval(1000).pipe(
-            map(value => ({ type: "COUNT", value })),
-            take(60 * 25)
-          ),
-        onDone: "idle"
-      },
-      on: {
-        COUNT: { actions: [assign((_, event) => ({ current: event.value }))] },
-        CANCEL: "idle",
-        STOP: "idle"
+    break: {
+      initial: "idle",
+      states: {
+        idle: {
+          on: {
+            PLAY: "running"
+          }
+        },
+        running: {
+          invoke: {
+            src: () =>
+              interval(1000).pipe(
+                map(value => ({ type: "COUNT", value })),
+                take(60 * 25)
+              ),
+            onDone: "#timer.tomato"
+          },
+          on: {
+            COUNT: { actions: [assign((_, event) => ({ current: event.value }))] },
+            CANCEL: "#timer.tomato",
+            STOP: "#timer.tomato"
+          }
+        }
       }
     }
   }
@@ -68,15 +99,18 @@ export function Timer() {
   const [state, send] = useMachine(timerMachine);
   const play = () => send("PLAY");
   const stop = () => send("STOP");
+
+  console.log(state.value);
   console.log(state.context.current, format(state.context.current, "mm:ss"));
+
+  const idle = state.matches("tomato.idle") || state.matches("break.idle");
+  const running = state.matches("tomato.running") || state.matches("break.running");
   return (
     <View>
       <div>{format(state.context.current, "mm:ss")}</div>
       <div>Task title</div>
-      {state.matches("idle") && <i onClick={play} dangerouslySetInnerHTML={{ __html: icons["play-circle"].toSvg() }} />}
-      {state.matches("running") && (
-        <i onClick={stop} dangerouslySetInnerHTML={{ __html: icons["stop-circle"].toSvg() }} />
-      )}
+      {idle && <i onClick={play} dangerouslySetInnerHTML={{ __html: icons["play-circle"].toSvg() }} />}
+      {running && <i onClick={stop} dangerouslySetInnerHTML={{ __html: icons["stop-circle"].toSvg() }} />}
     </View>
   );
 }
