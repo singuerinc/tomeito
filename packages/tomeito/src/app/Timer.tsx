f695877c38368e237117c0fcefa0aa17e5e2ae89
singuerinc <nahuel.scotti@gmail.com>
2020-05-10 17:02:03 +0200

    feat(timer): task input

--- packages/tomeito/src/app/Timer.tsx
+++ packages/tomeito/src/app/Timer.tsx
@@ -1,6 +1,6 @@
 import { useMachine } from "@xstate/react";
 import format from "date-fns/format";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { interval } from "rxjs";
 import { map, take } from "rxjs/operators";
 import styled from "styled-components";
@@ -8,13 +8,16 @@ import { assign, Machine } from "xstate";
 import { PlayButtonR } from "../icons/PlayButtonR";
 import { PlayStopR } from "../icons/PlayStopR";
 
+const TIMER_TIME = 60 * 25;
+const BREAK_TIME = 60 * 5;
+
 type Tomato = {
   dateCreated: Date;
   completed: boolean;
 };
 
 const context: TimerContext = {
-  current: 0,
+  current: TIMER_TIME,
   list: []
 };
 
@@ -29,16 +32,29 @@ export function Timer() {
 
   const idle = state.matches("tomato.idle") || state.matches("break.idle");
   const running = state.matches("tomato.running") || state.matches("break.running");
+  const isBreak = state.matches("break");
+  const total = isBreak ? BREAK_TIME : TIMER_TIME;
 
   const color = mapStateToColor(idle ? "idle" : "running");
 
+  const [task, setTask] = useState("Task");
+
+  const onTaskChange = (e: React.ChangeEvent<HTMLInputElement>) => setTask(e.target.value);
+
+  const ref = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    ref.current && (ref.current.style.width = 325 - 325 * (state.context.current / total) + "px");
+  }, [state.context.current]);
+
   return (
     <View color={color}>
+      <span ref={ref}></span>
       <div>
         <div>{time}</div>
       </div>
       <div>
-        <div>Task title</div>
+        {isBreak ? <div>Break!</div> : <input type="text" value={task} onChange={onTaskChange} />}
       </div>
       <div>
         {idle && <PlayButtonR onClick={play} />}
@@ -57,6 +73,7 @@ const timerMachine = Machine<TimerContext, TimerStateSchema, TimerEvent>({
       initial: "idle",
       states: {
         idle: {
+          entry: [assign(_ => ({ current: TIMER_TIME }))],
           on: {
             PLAY: "running"
           }
@@ -65,15 +82,17 @@ const timerMachine = Machine<TimerContext, TimerStateSchema, TimerEvent>({
           invoke: {
             src: () =>
               interval(1000).pipe(
-                map(value => ({ type: "COUNT", value })),
-                take(60 * 25)
+                map(() => ({ type: "COUNT" })),
+                take(TIMER_TIME)
               ),
             onDone: "#timer.break"
           },
           on: {
-            COUNT: { actions: [assign((_, event) => ({ current: event.value }))] },
+            COUNT: { actions: [assign(ctx => ({ current: ctx.current - 1 }))] },
             CANCEL: "#timer.break",
-            STOP: "#timer.break"
+            STOP: {
+              target: "#timer.break"
+            }
           }
         }
       }
@@ -82,6 +101,7 @@ const timerMachine = Machine<TimerContext, TimerStateSchema, TimerEvent>({
       initial: "idle",
       states: {
         idle: {
+          entry: [assign(_ => ({ current: BREAK_TIME }))],
           on: {
             PLAY: "running"
           }
@@ -90,15 +110,17 @@ const timerMachine = Machine<TimerContext, TimerStateSchema, TimerEvent>({
           invoke: {
             src: () =>
               interval(1000).pipe(
-                map(value => ({ type: "COUNT", value })),
-                take(60 * 25)
+                map(() => ({ type: "COUNT" })),
+                take(BREAK_TIME)
               ),
             onDone: "#timer.tomato"
           },
           on: {
-            COUNT: { actions: [assign((_, event) => ({ current: event.value }))] },
+            COUNT: { actions: [assign(ctx => ({ current: ctx.current - 1 }))] },
             CANCEL: "#timer.tomato",
-            STOP: "#timer.tomato"
+            STOP: {
+              target: "#timer.tomato"
+            }
           }
         }
       }
@@ -125,9 +147,9 @@ interface TimerStateSchema {
 
 type TimerEvent =
   | { type: "PLAY" }
-  | { type: "COUNT"; value: number }
+  | { type: "COUNT" }
   | { type: "CANCEL" }
-  | { type: "STOP" };
+  | { type: "STOP"; current: number };
 
 interface TimerContext {
   current: number;
@@ -135,21 +157,49 @@ interface TimerContext {
 }
 
 const View = styled.div<{ color: string }>`
+  box-sizing: border-box;
+  position: relative;
   display: flex;
   background-color: ${props => props.color};
-  width: 23em;
-  padding: 0.5em;
+  width: 325px;
+  padding: 5px;
   svg {
     color: white;
   }
 
+  > span {
+    z-index: 0;
+    background: rgba(0, 0, 0, 0.1);
+    width: 325px;
+    position: absolute;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+
   > div:nth-of-type(1) {
-    flex: 20%;
+    z-index: 1;
+    flex: 0 0 46px;
+    font-variant-numeric: tabular-nums;
   }
   > div:nth-of-type(2) {
-    flex: 1 1 100%;
+    z-index: 1;
+    flex: 1 1 auto;
+    padding: 0 5px;
+    input {
+      padding: 0;
+      margin: 0;
+      border: 0;
+      width: 100%;
+      font-size: 1em;
+      background: transparent;
+      &:focus {
+        outline-width: 0;
+      }
+    }
   }
   > div:nth-of-type(3) {
-    flex: 1 1;
+    z-index: 1;
+    flex: 0 0;
   }
 `;
